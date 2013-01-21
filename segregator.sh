2297ef5ad0a2cc5e3ae9f72b32972347f9f6ec05
Katarzyna Ścibek <kscibek@sigma.inf.ug.edu.pl>
2013-01-22 00:31:01 +0100

    test

--- segregator.sh
+++ segregator.sh
@@ -1,23 +1,101 @@
-\#!/bin/bash
+#!/bin/bash
 
 DIALOG=${DIALOG=dialog}
 $DIALOG --title "Segregator" --clear \
-   --yesno "Czy chcesz dokonac segregacji plikow w tym katalogu?" 10 40
+   --yesno "\n \n Czy chcesz dokonac segregacji plikow wg daty utworzenia w tym katalogu?" 9 40
 case $? in
 0)
-  echo "W porzadku";;
+$DIALOG --menu "Jak chcesz posegregowac pliki?" 12 40 3 1 "dziennie" 2 "wg miesiecy"
+
+case $? in 
+0)
+$DIALOG --title "Segregacja dzienna" --clear \
+   --yesno "\n \n       Skompresowac pliki?" 8 40
+case $? in
+0)
+  MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-30| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+
+ for i in `seq 2 $MAX`
+do
+   LINE=$i"p"
+        mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-30 | tr ' '$
+done
+rm -rf .zzz.txt
+
+zip -r segr-`date -u +%F` `ls | tr ' ' '\n'`;;
+
+
+1)
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-30| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+
+ for i in `seq 2 $MAX`
+do
+   LINE=$i"p"
+        mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-30 | tr ' '$
+done
+rm -rf .zzz.txt ;;
+
+
+255)
+  break; clear;;
+esac
+
+;;
+
+1)
+$DIALOG --title "Segregacja wg miesiecy" --clear \
+   --yesno "\n \n       Skompresowac pliki?" 8 40
+case $? in
+0)
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-27| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+
+ for i in `seq 2 $MAX`
+do
+   LINE=$i"p"
+        mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-27 | tr ' '$
+done
+rm -rf .zzz.txt
+
+zip -r segr-`date -u +%F` `ls | tr ' ' '\n'` ;;
+
+1)
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-27| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+
+ for i in `seq 2 $MAX`
+do
+   LINE=$i"p"
+        mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-27 | tr ' '$
+done
+rm -rf .zzz.txt ;;
+
+
+255)
+  break; clear;;
+esac
+;;
+
 1)
   break; clear;;
 255)
   break; clear;;
 esac
 
-dialog --menu "Wg jakiego kryterium chcesz posegregowac pliki?" 13 40 3 1 rozmiar 2 "data utworzenia" 3 nazwa
-clear
+
 
 <<COMMENT1
 po dniach z pakowaniem
-
 MAX=`ls -l |wc -l`
 ls -l >.zzz.txt
 mkdir `ls -l | cut -b 25-30| tr ' ' '-'` 2> /dev/null
@@ -47,6 +125,11 @@ do
 done
 rm -rf .zzz.txt
 COMMENT2
+
+
+
+
+
 
 <<COMMENT3
 PO MIESIĄCACH Z PAKOWANIEM
