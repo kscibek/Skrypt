a007ca114b4341abd705c2ba133a35281fd493b9
droszkiewicz <rogu666@gmail.com>
2013-01-21 17:19:03 +0100

    Update segregator.sh
    Dodałem skrypty sortujące. Żeby było łatwiej każdy jest w osobnym komentarzu. Aby działały usuń <<COMMENT# i COMMENT#.

--- segregator.sh
+++ segregator.sh
@@ -15,3 +15,70 @@ esac
 dialog --menu "Wg jakiego kryterium chcesz posegregowac pliki?" 13 40 3 1 rozmiar 2 "data utworzenia" 3 nazwa
 clear
 
+<<COMMENT1
+po dniach z pakowaniem
+
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-30| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+ 
+ for i in `seq 2 $MAX` 
+do
+   LINE=$i"p"
+	mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-30 | tr ' ' '-'` 2> /dev/null
+done
+rm -rf .zzz.txt
+
+zip -r segr-`date -u +%F` `ls | tr ' ' '\n'`
+
+COMMENT1
+<<COMMENT2
+po dniach bez pakowania
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-30| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+ 
+ for i in `seq 2 $MAX` 
+do
+   LINE=$i"p"
+	mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-30 | tr ' ' '-'` 2> /dev/null
+done
+rm -rf .zzz.txt
+COMMENT2
+
+<<COMMENT3
+PO MIESIĄCACH Z PAKOWANIEM
+
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-27| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+ 
+ for i in `seq 2 $MAX` 
+do
+   LINE=$i"p"
+	mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-27 | tr ' ' '-'` 2> /dev/null
+done
+rm -rf .zzz.txt
+
+zip -r segr-`date -u +%F` `ls | tr ' ' '\n'`
+COMMENT3
+
+<<COMMENT4
+PO MIESIACACH BEZ PAKOWANIA
+
+MAX=`ls -l |wc -l`
+ls -l >.zzz.txt
+mkdir `ls -l | cut -b 25-27| tr ' ' '-'` 2> /dev/null
+cat .zzz.txt
+ 
+ for i in `seq 2 $MAX` 
+do
+   LINE=$i"p"
+	mv `cat <.zzz.txt| sed -n $LINE| cut -b 38-`  --target-directory=`cat <.zzz.txt |sed -n $LINE| cut -b 25-27 | tr ' ' '-'` 2> /dev/null
+done
+rm -rf .zzz.txt
+
+COMMENT4
